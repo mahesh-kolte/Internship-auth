a939884229b8469bdc00592ed1d8bfa422e8a785
mahesh-kolte <maheshkolte73@gmail.com>
2026-06-11 09:50:21 +0530

    Frontend deployment ready

--- frontend/src/Services/api.js
+++ frontend/src/Services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api/auth",
+  baseURL: " https://internship-auth.onrender.com",
 });
 
 export default API;
